b918d9f4a99452e1c5eefb0f4d7ed02f8f607dce
Kabeer Hussain <shigristudy@gmail.com>
2023-02-07 19:01:52 +0500

    Changes

--- src/components/Basket.vue
+++ src/components/Basket.vue
@@ -269,7 +269,7 @@
               programmes.
             </p>
           </div>
-          <div class="mb-2">
+          <div class="mb-2 flex">
             <input
               id="agreed"
               type="checkbox"
@@ -312,7 +312,7 @@
           </div>
         </div>
         <hr>
-        <div class="my-2">
+        <div class="my-2 flex">
           <input
             id="synergidigital-accept-terms-business"
             type="checkbox"
@@ -398,7 +398,9 @@
               Back
 
             </button>
-            <button type="button" @click="moveForward()" class="px-2 md:px-6 pt-2.5 pb-2 bg-red text-white font-medium text-xs leading-normal uppercase rounded shadow-md hover:bg-green dark:hover:bg-black hover:shadow-lg focus:bg-green dark:focus:bg-black focus:shadow-lg focus:outline-none focus:ring-0 dark:active:bg-black active:bg-green active:shadow-lg transition duration-300 ease-in-out flex align-center items-center">
+            <!-- :class="{''}" -->
+            <button type="button" @click="moveForward()"
+              class="px-2 md:px-6 pt-2.5 pb-2 bg-red text-white font-medium text-xs leading-normal uppercase rounded shadow-md hover:bg-green dark:hover:bg-black hover:shadow-lg focus:bg-green dark:focus:bg-black focus:shadow-lg focus:outline-none focus:ring-0 dark:active:bg-black active:bg-green active:shadow-lg transition duration-300 ease-in-out flex align-center items-center">
               Proceed to Payment
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 ml-3">
               <path stroke-linecap="round" stroke-linejoin="round" d="M12.75 15l3-3m0 0l-3-3m3 3h-7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
